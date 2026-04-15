00399e8b76e9aef674824f5c49a518a913417dbf
Zackary Simon <zacksimon13@gmail.com>
2026-04-14 20:18:13 -0400

    navbar scrolling fix #1

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,37 @@
 console.log("Zack's builder site loaded");
 
-// Keep hotbar always visible
 (function () {
+  const navbar = document.querySelector(".navbar");
   const hotbar = document.querySelector(".hotbar");
+  const navContact = document.querySelector(".nav-contact");
+
+  if (!navbar) return;
+
+  const compactThreshold = 36;
+
+  function setCompactNavbar(isCompact) {
+    navbar.classList.toggle("navbar-compact", isCompact);
+
+    [hotbar, navContact].forEach((element) => {
+      if (!element) return;
+
+      if (isCompact) {
+        element.setAttribute("aria-hidden", "true");
+      } else {
+        element.removeAttribute("aria-hidden");
+      }
+    });
+  }
+
+  function syncNavbarState() {
+    setCompactNavbar(window.scrollY > compactThreshold);
+  }
+
   if (!hotbar) return;
 
   hotbar.classList.remove("hide", "closed");
   hotbar.removeAttribute("aria-hidden");
+
+  syncNavbarState();
+  window.addEventListener("scroll", syncNavbarState, { passive: true });
 })();
